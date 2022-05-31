b9fff0b71d0519f0d5d592d4eccb730a6513027c
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-05-31 17:21:35 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -18,18 +18,29 @@ const port = 3000;
 //       Tip Calculator 
 //-----------------------------
 
-app.get('/tip/:total/:percentage', (req, res) => {
-    let subtotal = req.params.total
-    let percent = req.params.percentage
+// app.get('/tip/:total/:percentage', (req, res) => {
+//     let subtotal = req.params.total
+//     let percent = req.params.percentage
 
-    let tip = subtotal * (percent/100)
-    let total = tip + subtotal
+//     let tip = subtotal * (percent/100)
+//     let total = tip + subtotal
 
-    res.send("Your total with tip will be " + total)
+//     res.send("Your total with tip will be " + total)
+// })
+
+
+//-----------------------------
+//       Magic 8-ball 
+//-----------------------------
+
+const response = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+    
+    let ballSays = response[Math.floor(Math.random() * response.length)];
+
+    res.send(ballSays)
 })
-
-
-
 
 
 
