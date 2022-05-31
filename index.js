b7c87d90ed0bb30a5505b1d070e597125386e442
annakoanna <anna.231991k@gmail.com>
2022-05-31 13:55:10 -0700

    Tip Calculator express application created.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const port = 3000;
 app.get('/greeting/:name', (req, res)=>{
     res.send(`hello, ${req.params.name}` )
 })
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    let total = parseInt(req.params.total)
+    let tipPercentage = parseInt(req.params.tipPercentage)
+    let tip = (tipPercentage / 100) * total
+    res.send(`${tip}`)
+    })
 app.listen(port, () => {
     console.log('app is running on port: ', port);
 });
