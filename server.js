685f6799dc209827128505d1d72dd236c38496a4
doorstrawberry <kangminsoo671@gmail.com>
2022-05-31 15:21:17 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -16,10 +16,18 @@ app.get("/greeting/:name", (request, response) => {
     response.send(`${personName}! It's so great to see you!`)
 })
 
-// Makinga tip calculator
+// Making a tip calculator
 app.get("/tip/:total/:tipPercentage", (request, response) => {
     const bill = Number(request.params.total)
     const tip = Number(request.params.tipPercentage)
     let tipAmount = Number(tip/bill*100)
     response.send(`Tip Amount: ${tipAmount}`)
+})
+
+// Magic 8 Ball
+app.get("/magic/:question", (request, response) => {
+    const randomResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    let randomResponse = randomResponses[Math.floor(Math.random()*randomResponses.length)]
+    let question = String(request.params.question)
+    response.send(`<h1>${question}: ${randomResponse}</h1>`)
 })
