8a44312dedbcd63d6dc8cd6ea7d1323eade229c2
doorstrawberry <kangminsoo671@gmail.com>
2022-05-31 15:00:03 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -15,3 +15,11 @@ app.get("/greeting/:name", (request, response) => {
     const personName = request.params.name
     response.send(`${personName}! It's so great to see you!`)
 })
+
+// Makinga tip calculator
+app.get("/tip/:total/:tipPercentage", (request, response) => {
+    const bill = Number(request.params.total)
+    const tip = Number(request.params.tipPercentage)
+    let tipAmount = Number(tip/bill*100)
+    response.send(`Tip Amount: ${tipAmount}`)
+})
