2d44522c28d64d903f8975904076e28d5d02c0e3
doorstrawberry <kangminsoo671@gmail.com>
2022-05-31 14:45:04 -0400

    Greeting express application created

--- server.js
+++ server.js
@@ -10,3 +10,8 @@ app.listen(port, () => {
     console.log("Using port: ", port)
 })
 
+// Making a route for greeting
+app.get("/greeting/:name", (request, response) => {
+    const personName = request.params.name
+    response.send(`${personName}! It's so great to see you!`)
+})
