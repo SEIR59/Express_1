801d60acc1acf93cb1145a4468d9031544e0091b
earlhalasan <earl.halasan@gmail.com>
2022-05-31 17:48:59 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -17,3 +17,31 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     `A ${req.params.tipPercentage}% tip of $${req.params.total} is $${tip}.`
   );
 });
+
+let eightBall = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
+app.get("/magic/:question", (req, res) => {
+  let answer = eightBall[Math.floor(Math.random() * eightBall.length)];
+  res.send(`${req.params.question} <h1>${answer}</h1>`);
+});
