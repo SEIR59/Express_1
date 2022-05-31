001e8c943146ad09019eb556fcaf3b5b3c29eeff
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-05-31 17:01:05 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const app = express();
-
 const port = 3000;
 
-app.get('/greeting/:name', (req, res) => {
-    let person = req.params.name
+//-----------------------------
+//         Greetings
+//-----------------------------
+
+// app.get('/greeting/:name', (req, res) => {
+//     let person = req.params.name
     
-    res.send("Hello There, " + person)
+//     res.send("Hello There, " + person)
+// })
+
+
+
+//-----------------------------
+//       Tip Calculator 
+//-----------------------------
+
+app.get('/tip/:total/:percentage', (req, res) => {
+    let subtotal = req.params.total
+    let percent = req.params.percentage
+
+    let tip = subtotal * (percent/100)
+    let total = tip + subtotal
+
+    res.send("Your total with tip will be " + total)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(port, () => {
