f8a653b418dae20c19b908a7ec6fa06417f4e415
heysungj <linjing0525@gmail.com>
2022-05-31 15:06:17 -0400

    tip commit

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.get("/greeting/:name", (req, res) => {
   res.send(`Hello there , ${req.params.name}`);
 });
 
+app.get("/tip/:total/:percentage", (req, res) => {
+  let total = parseFloat(req.params.total);
+  let percentage = parseFloat(req.params.percentage) / 100;
+  let tip = total * percentage;
+  res.send(`The tip is : ${tip}`);
+});
+
 app.listen(port, () => {
   console.log("app is running on port", port);
 });
