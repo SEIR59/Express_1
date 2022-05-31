17359c674ff885cfd87290e28ff9a253e317d668
Mark Gerken <mgerken86@gmail.com>
2022-05-31 13:21:01 -0600

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// ******** GREETING ***********
+
 app.get('/greeting/:name', (req, res) => {
     name = req.params.name
     res.send(`Hello, ${name}`)
 })
+
+// ******** TIP CALCULATOR *********
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    grandTotal = Number(req.params.total) - (Number(req.params.total) *( Number(req.params.tipPercentage) * .01))
+    res.send(`Your total before tax was ${req.params.total}. The tip percentage is ${req.params.tipPercentage}. Your grand total after tax is ${grandTotal}`)
+})
+
 
 app.listen(port, () => {
     console.log('app is running on port: ', port)
