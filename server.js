b2cb636d2423e83cf2dce7cf1198502909dc2fa5
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-05-31 17:45:25 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -25,3 +25,16 @@ let tip = parseInt(req.params.total)*(parseInt(req.params.tipPercentage)/100)
 
 res.send(tip.toString())
 })
+
+//===== Magic 8 ball =====//
+
+
+
+app.get('/magic/:question/',(req,res) => {
+    
+    const response = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    
+    let random = Math.floor(Math.random(response.length -1))
+    
+    res.send(req.params.question + "? "  + "\n"+ response[random])
+    })
