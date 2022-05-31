e340e1c7194747d7695a4610100ddacc9e287f63
earlhalasan <earl.halasan@gmail.com>
2022-05-31 17:24:53 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ app.listen(port, () => {
 app.get("/greeting/:name", (req, res) => {
   res.send(`Hello, ${req.params.name}!`);
 });
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  tip = parseInt(req.params.total) * (parseInt(req.params.tipPercentage) / 100);
+  res.send(
+    `A ${req.params.tipPercentage}% tip of $${req.params.total} is $${tip}.`
+  );
+});
