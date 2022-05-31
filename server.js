0b76471695c1549e0370715f2c199fea7ec2016d
jrbreeden <jrbreeden55@gmail.com>
2022-05-31 19:04:26 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.get('/greeting/:name', (req, res) => {
     res.send('hello' + req.params.name)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    console.log(req)
+    // console.log(req.params)
+    let decimal = req.params.tipPercentage / 100 
+    let tip = req.params.total * decimal
+    res.send('tip: ' + tip)
+})
 
 
 
