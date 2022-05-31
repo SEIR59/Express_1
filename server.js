12036558d84a8a64fac0467d20223b4928dfc874
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-05-31 16:00:40 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+// Greetings
 app.get("/greeting/:name", (req, res) => {
   res.send("Hello, " + req.params.name);
 });
+// Tip Calculator
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  let result =
+    (Number(req.params.total) * Number(req.params.tipPercentage)) / 100;
+  res.send(`${result}`);
+});
 
 app.listen(port, () => {
-  console.log(`port: ` + port);
+  console.log("port: " + port);
 });
