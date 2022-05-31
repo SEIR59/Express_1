17b7fc3eb552235a28dc1e61feecdd3534d666a6
patrickbasham <PatrickTBasham@gmail.com>
2022-05-31 17:35:38 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const app = expr()
 
 const port = 3000
 
+// default webpage.
 app.get("/", (req, res) => {
     res.send("This is the default route of this project!")
 })
 
+// Greetings
+// greeting route that will display a random greeting.
 app.get("/greeting/:name", (req, res) => {
 
     // get one of 4 greetings
@@ -20,6 +23,17 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`${randomGreeting}`)
 })
 
+// Tip Calculator - 
+//gets the requested tip percentage of a given total.
+app.get("/tip/:total/:tipPercentage", (req,res) => {
+    let total = parseInt(req.params.total)
+    let tipPercentage = parseInt(req.params.tipPercentage)
+    // calculate the tip percentage and make sure it rounds correctly. No leading numbers!
+    let tip = (tipPercentage / total) * 100
+    tip = Math.round((tip + Number.EPSILON) * 100) / 100
+
+    res.send(`${tip}`)
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
