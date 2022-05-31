ff09f6a4f8f1a0953b4eafc315df2090c4f35572
hnhtran <tran_eetas@yahoo.com>
2022-06-01 06:06:28 +1000

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -9,3 +9,10 @@ app.listen(port, () => {
 app.get(`/greeting/:name`, (req, res) => {
     res.send(`Greeting ${req.params.name}`)
 })
+
+app.get(`/tip/:bill/:pct`, (req, res) => {
+    const pct = req.params.pct
+    const bill = req.params.bill
+    let tip = bill * pct / 100
+    res.send(`Heres your tip ${tip}`)
+})
