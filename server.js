05ee47c2098f92342b2808e4351c334bc32a6357
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-05-31 18:49:40 -0400

    99 bottles complete

--- server.js
+++ server.js
@@ -33,13 +33,33 @@ const port = 3000;
 //       Magic 8-ball 
 //-----------------------------
 
-const response = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+// const response = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
-app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+// app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
     
-    let ballSays = response[Math.floor(Math.random() * response.length)];
+//     let ballSays = response[Math.floor(Math.random() * response.length)];
 
-    res.send(ballSays)
+//     res.send(ballSays)
+// })
+
+
+//-----------------------------
+//Take one Down and Pass it Around
+//-----------------------------
+
+app.get('/:bottles',(req,res)=>{
+    
+    let bottles = parseInt(req.params.bottles)
+    let startOvr = (`<a href="/99" >NO MORE BOTTLES OF BEER</a>`)
+    let nextBtn = (`<a href="/${bottles-1}" >Take one down, pass it around </a>`)
+    
+    if(bottles >0) {
+        res.send(`<h1>${bottles} bottles of beer on the wall ${bottles} bottles of beer, ${nextBtn}</h1>`)
+    }
+    else {
+        res.send(`<H1>${startOvr}</H1>`)
+    }
+    
 })
 
 
