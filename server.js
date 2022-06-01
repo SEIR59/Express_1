b52e22e2f8f84d788996c90fa1ce23ab58c83299
patwyzlic <patrickwyzlic@gmail.com>
2022-06-01 01:06:59 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const app = expr();
 
 app.listen(3000);
 
+//Greeting express app created
 app.get('/greeting/:name', (req, res) => {
     let name = req.params.name;
     res.send(`What's up ${name}, it's great to see you!`);
+});
+
+//Tip Calculator
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let total = parseFloat(req.params.total);
+    let tipPercentage = parseFloat(req.params.tipPercentage);
+    let theTip = total * (.01 * tipPercentage);
+    res.send(`${theTip}`);
 });
