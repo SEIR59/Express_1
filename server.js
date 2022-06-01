ef41e284e1dd9a8548e001497e017d7ebee65a7d
Hoskyfull <norberto.cardenales@gmail.com>
2022-06-01 15:17:41 -0600

    Pass it around express created

--- server.js
+++ server.js
@@ -29,13 +29,26 @@ const app = express();
 
 //MAGIC 8 BALL
 
-app.get('/magic/:phrase', (req,res) =>{
+// app.get('/magic/:phrase', (req,res) =>{
 
-    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+//     const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
-    randomResponse = Math.floor(Math.random() * responses.length)
+//     randomResponse = Math.floor(Math.random() * responses.length)
 
-    res.send(`You asked the magic ball ${req.params.phrase} and replied:<br>${responses[randomResponse]}`)
+//     res.send(`You asked the magic ball ${req.params.phrase} and replied:<br>${responses[randomResponse]}`)
+// })
+
+
+app.get('/', (req,res) => {
+    res.send(`<h1> 99 Bottles of beer on the wall <br> <a href="/98">testing</a></h1>`)
+})
+
+app.get('/:number_of_bottles', (req, res) =>{
+    if(req.params.number_of_bottles === '0'){
+        res.send(`<h1><a href="/"> Start Over </a></h1>`)
+    } else {
+        res.send(`<h1>${req.params.number_of_bottles} Bottles of beer on the wall <br><a href= ${req.params.number_of_bottles - 1}>Take one down, pass it around</a></h1>`)
+    }
 })
 
 
