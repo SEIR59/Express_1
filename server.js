51266f74cc2302db5dcd6db2985f112b4c9e2854
mkhan42 <michallekhan9@gmail.com>
2022-05-31 20:19:40 -0400

    magic 8 ball answered

--- server.js
+++ server.js
@@ -35,6 +35,31 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`The total bill is $${billTotal}, the tip percentage is ${tipPercent}%, the calculated tip is $${calculatedTip}, and the overall total is $${tipBillTotal}`)
 })
 
+/*
+## Magic 8 Ball
+
+2. Create a route of `'/magic'` that should expect a phrase in the URL that ask the Magic 8 ball a question.
+
+3.  So if the user hits that route and asks a question of "Will I be a Millionaire" (ex. `'/magic/Will%20I%20Be%20A%20Millionaire'`) he should have return to him his question AND a random Magic 8 ball response (see the array below) on the  screen.
+
+4. We can't use spaces in the url, so we use `%20` to express a space in the url.
+
+5. So if the user hits that route and asks a question of "Will I be a Millionaire" he should get his question asked and a random Magic 8 ball quote on the  screen.
+- Send the magic 8 ball response back between html `<h1>` tags
+- Use this array of Magic 8 ball responses.....
+
+```
+["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+```
+*/
+
+app.get('/magic/:question', (req, res) => {
+    const magicArr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+    let answer = magicArr[Math.floor(Math.random()*magicArr.length)]
+    res.send(`${req.params.question}? \n ${answer}`)
+})
+
+
 app.listen(port, () => {
 	console.log('app is running on port: ', port)
 });
