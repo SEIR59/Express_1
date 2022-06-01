451674e4a799675b6f0ec125a03121d275b34875
“GissellValdez” <Gissellvaldez@gmail.com>
2022-05-31 22:35:20 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const app = express()
 
 const port = 3000
 
-// Greeting
+// Greetings
 
 app.get('/greeting/:name', (req, res) => {
 	name = req.params.name
 	res.send(`Hello, ${name}`)
 
+})
+
+
+//Tip Calculator
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+	// console.log("this is working")
+	let total = parseInt(req.params.total)
+	let tipPercentage = parseInt(req.params.tipPercentage)
+	let tipTotal = total*(tipPercentage / 100)
+	res.send(`The tip should be ${tipTotal}`)
 })
