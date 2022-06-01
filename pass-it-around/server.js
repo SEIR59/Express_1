dc96ce4e17465ff6cccafeaf7f8bdadb10f93f39
jrbreeden <jrbreeden55@gmail.com>
2022-05-31 21:27:31 -0400

    pass-it-around complete

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -2,13 +2,19 @@ const expr = require('express')
 const app = expr()
 
 
-let count = 99
+
 app.get('/', (req, res) => {
-
-
-
+    res.send("99 Bottles of beer on the wall" + `<a href="/98">take one down, pass it around</a>`)
 })
 
+app.get("/:number_of_bottles", (req, res) => {
+    let count = Number(req.params.number_of_bottles)
+    if (count === 0) {
+        res.send(`<a href= '/'>start over</a>`)
+    } else {
+        res.send(`${count} Bottles of beer on the wall` + `<a href="/${count - 1}">take one down, pass it around</a>`)
+    }
+})
 
 
 
