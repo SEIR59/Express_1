132ab883294a4a742df75bbc88300e2683a7fd3a
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-01 06:02:54 -0400

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -11,6 +11,37 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
   res.send(`Tip: $${(total * tipPercentage) / 100}`);
 });
 
+app.get('/magic/:question', (req, res) => {
+  const magicBallResponses = [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes definitely',
+    'You may rely on it',
+    'As I see it yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+    'Reply hazy try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    "Don't count on it",
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+  ];
+
+  res.send(
+    `<h1>${
+      magicBallResponses[Math.floor(Math.random() * magicBallResponses.length)]
+    }</h1>`
+  );
+});
+
 app.listen(3000, () => {
   console.log(`Server is running on port ${port}`);
 });
