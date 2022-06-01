b8a769c096ae7f092613fccf18b7dbb34148e785
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-01 06:02:14 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
-
-
 app.get('/greeting/:name', (req, res) => {
-    res.send(`Hello, ${req.params.name}`);
-  });
+  res.send(`Hello, ${req.params.name}`);
+});
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  let { total, tipPercentage } = req.params;
+  res.send(`Tip: $${(total * tipPercentage) / 100}`);
+});
 
 app.listen(3000, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  console.log(`Server is running on port ${port}`);
+});
