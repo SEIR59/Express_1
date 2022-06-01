f549cfb7452a81c56d43d63f554fb792766af2a8
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-05-31 20:38:47 -0400

    bonus

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -7,18 +7,19 @@ app.listen(port, (req,res) => {
 })
 
 app.get('/',(req,res) => {
-    res.send(" 99 bottles of beer on the wall!<br/>"+`<a href="/98"> Take one down, pass it around</a>`)
-
+    // res.send(" 99 bottles of beer on the wall!<br/>"+`<a href="/98"> Take one down, pass it around</a>`)
+    res.send(" 99 little bugs in the code<br/>99 little bugs<br/>Take on down<br/>Patch it around<br/>127 bugs in the code<br/>"+`<a href="/98"> Take one down, pass it around</a>`)
 })
 
 
 
 app.get('/:number_of_bottles',(req,res) => {
     let number = parseInt(req.params.number_of_bottles)
+    let random = Math.floor(Math.random()*30+1)
   if(number === 0){
       res.send(`<a href="/">Going back to Home</a>`)
   }else{
-    res.send( number + " Bottles of beer on the wall!<br/> "+ `<a href='/${number-1}'> Take one down, pass it around</a>`)
+    res.send( number + " little bugs in the code<br/>"+ `${number}little bugs<br/>Take on down<br/>Patch it around<br/>` + `${random+127}bugs in the code<br/>`+`<a href='/${number-1}'> Take one down, pass it around</a>`)
   }
 })
 
