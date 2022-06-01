79fae663392bd0cdb6c4a7d1a8a8e9aab8c34acb
patwyzlic <patrickwyzlic@gmail.com>
2022-06-01 01:18:11 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -17,3 +17,12 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     let theTip = total * (.01 * tipPercentage);
     res.send(`${theTip}`);
 });
+
+//Magic 8 ball
+const eightBallArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+app.get('/magic/:phrase', (req, res) => {
+    let newRand = Math.floor(Math.random() * eightBallArray.length);
+    let thePhrase = req.params.phrase;
+    let replaced = thePhrase.replace("%20", " ");
+    res.send(`${replaced}: <h1>${eightBallArray[newRand]}</h1>`);
+});
