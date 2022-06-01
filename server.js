66a7b13cc58495b5439098d746a29b5cd67a70f1
NSTaft <taftnathan13@gmail.com>
2022-05-31 19:21:37 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`Hello, ${req.params.name}. How the heck are you?`)
 })
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send(`The total bill is $${req.params.total} and a 20% tip will be $${req.params.tipPercentage}.`)
+})
+
 app.listen(3000, () => {
     console.log("Working")
 })
